6924cbd858c55f48efde2ce850f7a0fae20475f9
Aryan Zaky Prayogo <prayogoaryan63@gmail.com>
2025-07-22 22:25:16 +0700

    Update page.js

--- frontend/app/admin/dashboard/page.js
+++ frontend/app/admin/dashboard/page.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 export default function AdminDashboard() {
+  const [showAddModal, setShowAddModal] = useState(false);
   // Fungsi untuk membuka modal tambah kandidat
   const handleAddCandidate = () => {
     setCandidateForm({
